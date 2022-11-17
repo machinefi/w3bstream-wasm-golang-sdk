22d5b55f723e5bb093f170fbdadc94f559a9b0d0
Liuhaai <haixiang@iotex.io>
2022-11-17 14:41:10 -0800

    add log

---
+++ log/log.go
@@ -0,0 +1,10 @@
+package log
+
+import (
+	"github.com/machinefi/w3bstream-wasm-golang-sdk/common"
+)
+
+func Log(message string) {
+	ptr, size := common.StringToPointer(message)
+	_ = common.WS_log(3, ptr, size) // logInfoLevel = 3
+}
