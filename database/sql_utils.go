b407ffffa8f865a421d19c676444609f80f77e7a
Liuhaai <haixiang@iotex.io>
2022-12-06 16:34:21 -0800

    add sql

---
+++ database/sql_utils.go
@@ -0,0 +1,95 @@
+package database
+
+import (
+	"encoding/base64"
+)
+
+type (
+	dBQuery struct {
+		Statement string   `json:"statement"`
+		Params    []*param `json:"params"`
+	}
+
+	param struct {
+		Int32   int32   `json:"int32,omitempty"`
+		Int64   int64   `json:"int64,omitempty"`
+		Float32 float32 `json:"float32,omitempty"`
+		Float64 float64 `json:"float64,omitempty"`
+		String  string  `json:"string,omitempty"`
+		Time    string  `json:"time,omitempty"` //  rfc3339 encoding
+		Bool    bool    `json:"bool,omitempty"`
+		Bytes   string  `json:"bytes,omitempty"` // base64 encoding
+	}
+)
+
+type sqlInt32 struct {
+	data int32
+}
+
+func (str *sqlInt32) getParam() *param {
+	return &param{
+		Int32: str.data,
+	}
+}
+
+type sqlInt64 struct {
+	data int64
+}
+
+func (str *sqlInt64) getParam() *param {
+	return &param{
+		Int64: str.data,
+	}
+}
+
+type sqlFloat32 struct {
+	data float32
+}
+
+func (str *sqlFloat32) getParam() *param {
+	return &param{
+		Float32: str.data,
+	}
+}
+
+type sqlFloat64 struct {
+	data float64
+}
+
+func (str *sqlFloat64) getParam() *param {
+	return &param{
+		Float64: str.data,
+	}
+}
+
+type sqlString struct {
+	data string
+}
+
+func (str *sqlString) getParam() *param {
+	return &param{
+		String: str.data,
+	}
+}
+
+type sqlBool struct {
+	data bool
+}
+
+func (str *sqlBool) getParam() *param {
+	return &param{
+		Bool: str.data,
+	}
+}
+
+type sqlBytes struct {
+	data []byte
+}
+
+func (str *sqlBytes) getParam() *param {
+	return &param{
+		Bytes: base64.StdEncoding.EncodeToString(str.data),
+	}
+}
+
+// TODO support time
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/machinefi/w3bstream-wasm-golang-sdk
 
 go 1.18
+
+require github.com/mailru/easyjson v0.7.7
+
+require github.com/josharian/intern v1.0.0 // indirect
