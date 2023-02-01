f2186c123fc7d57d1f63e501581fa9a25d0a4085
Liuhaai <haixiang@iotex.io>
2023-02-01 14:41:24 -0800

    update abi & clean code

--- examples/token_distribute/token_distribute.go
+++ examples/token_distribute/token_distribute.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"math/big"
 
+	"github.com/machinefi/w3bstream-wasm-golang-sdk/blockchain"
 	"github.com/machinefi/w3bstream-wasm-golang-sdk/database"
 	"github.com/machinefi/w3bstream-wasm-golang-sdk/log"
 	"github.com/machinefi/w3bstream-wasm-golang-sdk/stream"
@@ -19,21 +23,33 @@ func _start(rid uint32) int32 {
 		return -1
 	}
 	log.Log(fmt.Sprintf("receive message: %s", res))
-	count := database.GetDB("clicks") + 1
+	count := getDB("clicks") + 1
 	if count%5 == 0 {
-		/*
-			common.SendTx(fmt.Sprintf(
-				`{
-					"to": "%s",
-					"value": "0",
-					"data": "40c10f19000000000000000000000000%s0000000000000000000000000000000000000000000000000de0b6b3a7640000"
-				}`,
-				"0x1ED83F5AD999262eC06Ed8f3B801e108024b3e9c",
-				"97186a21fa8e7955c0f154f960d588c3aca44f14",
-			))
-		*/
+		blockchain.SendTx(
+			4690,
+			"0x1ED83F5AD999262eC06Ed8f3B801e108024b3e9c",
+			big.NewInt(0),
+			fmt.Sprintf("40c10f19000000000000000000000000%s0000000000000000000000000000000000000000000000000de0b6b3a7640000",
+				"97186a21fa8e7955c0f154f960d588c3aca44f14"))
 		log.Log("send tx")
 	}
-	database.SetDB("clicks", count)
+	setDB("clicks", count)
 	return 0
+}
+
+func getDB(key string) int32 {
+	val, err := database.Get(key)
+	if err != nil {
+		return 0
+	}
+	var ret int32
+	buf := bytes.NewBuffer(val)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return ret
+}
+
+func setDB(key string, val int32) {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, val)
+	_ = database.Set(key, buf.Bytes())
 }
--- examples/word_count/word_count.go
+++ examples/word_count/word_count.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"strings"
 
@@ -13,25 +15,54 @@ func main() {}
 
 //export start
 func _start(rid uint32) int32 {
-	log.Log(fmt.Sprintf("start rid: %d", rid))
+	log.Log(fmt.Sprintf("start received: %d", rid))
 	str, err := stream.GetDataByRID(rid)
 	if err != nil {
-		log.Log("error: " + err.Error())
+		log.Log("error:" + err.Error())
 		return -1
 	}
 
 	words := strings.Split(string(str), " ")
-	records := make(map[string]int32)
+	counts := make(map[string]int32)
 	for _, w := range words {
-		if _, ok := records[w]; !ok {
-			records[w] = database.GetDB(w) + 1
+		if _, ok := counts[w]; !ok {
+			counts[w] = getDB(w) + 1
 		} else {
-			records[w]++
+			counts[w]++
 		}
 	}
 
-	for k, cnt := range records {
-		database.SetDB(k, cnt)
+	for k, cnt := range counts {
+		setDB(k, cnt)
+		if _, ok := records[k]; !ok {
+			records[k] = cnt
+		} else {
+			records[k] += cnt
+		}
 	}
 	return 0
+}
+
+//export word_count
+func _unique(_ uint32) int32 {
+	return int32(len(records))
+}
+
+var records = make(map[string]int32)
+
+func getDB(key string) int32 {
+	val, err := database.Get(key)
+	if err != nil {
+		return 0
+	}
+	var ret int32
+	buf := bytes.NewBuffer(val)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return ret
+}
+
+func setDB(key string, val int32) {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, val)
+	_ = database.Set(key, buf.Bytes())
 }
