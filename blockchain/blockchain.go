8076a32d255b9216cfb342414e08534fd0db40f8
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2023-05-09 14:11:36 +0800

    Revert "feat(blockchain): support sendTxWithPrivateKey wasm abi (#23)" (#24)

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -36,34 +36,6 @@ func SendTx(chainID uint32, to string, value *big.Int, data string) (string, err
 	return string(m.Data), nil
 }
 
-func SendTxWithPrivateKey(chainID uint32, to string, value *big.Int, data, privateKey string) (string, error) {
-	str := fmt.Sprintf(fmt.Sprintf(
-		`{
-			"to": "%s",
-			"value": "%s",
-			"data": "%s",
-			"privateKey": "%s"
-		}`,
-		to,
-		value.String(),
-		strings.TrimPrefix(data, "0x"),
-		strings.TrimPrefix(privateKey, "0x"),
-	))
-	addr, size := common.StringToPointer(str)
-
-	vaddr := uintptr(unsafe.Pointer(new(uint32)))
-	vsize := uintptr(unsafe.Pointer(new(uint32)))
-	if ret := common.WS_send_tx_with_private_key(chainID, addr, size, uint32(vaddr), uint32(vsize)); ret != 0 {
-		return "", errors.New("sent tx with private key failed")
-	}
-
-	m := common.Allocations.GetByAddr(*(*uint32)(unsafe.Pointer(vaddr)))
-	if m == nil {
-		return "", errors.New("sent tx with private key failed")
-	}
-	return string(m.Data), nil
-}
-
 func CallContract(chainID uint32, to string, data string) ([]byte, error) {
 	str := fmt.Sprintf(fmt.Sprintf(
 		`{
